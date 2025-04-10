f085d53ee6da6aa2d0f93a15a3e51906e657f387
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-10 21:23:42 +0800

    feat: add boilerplate code

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,1 +1,14 @@
 package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("thoth.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to init database")
+	}
+	return db
+}
