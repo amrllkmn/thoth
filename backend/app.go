e6b0f16e8fcc87dba543b602ee4d71ce81f75e52
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-10 22:43:29 +0800

    feat: add /health endpoint

--- backend/app.go
+++ backend/app.go
@@ -1,1 +1,23 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+type App struct {
+	Router *gin.Engine
+}
+
+func CreateApp() App {
+	router := gin.Default()
+	return App{
+		Router: router,
+	}
+}
+
+func (app *App) Run() {
+	app.Router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	app.Router.Run(":8080")
+}
