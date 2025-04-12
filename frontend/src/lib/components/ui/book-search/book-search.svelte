03adaa8bf94b37fb74faa2b66c4373d63c74bfc6
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-12 20:27:08 +0800

    feat: add empty result

--- frontend/src/lib/components/ui/book-search/book-search.svelte
+++ frontend/src/lib/components/ui/book-search/book-search.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { SearchBar } from '$lib/components/ui/search-bar';
+	import { EmptyPage } from '../empty-page';
 	import { EngineToggle } from '../engine-toggle';
 </script>
 
 <div class="space-y-4 sm:space-y-6">
 	<SearchBar />
 	<EngineToggle />
+	<EmptyPage />
 </div>
---
+++ frontend/src/lib/components/ui/empty-page/empty-page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center py-8 text-center sm:py-12">
+	<h3 class="text-base font-medium sm:text-lg">No books found</h3>
+	<p class="mt-1 text-xs text-muted-foreground sm:text-sm">Try again with a different query</p>
+</div>
