c69b87fee570482d893610ce65d0e559584e6886
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-12 19:49:26 +0800

    fix: space out component

--- frontend/src/lib/components/ui/title/title.svelte
+++ frontend/src/lib/components/ui/title/title.svelte
@@ -2,8 +2,8 @@
 	import { Logo } from '$lib/components/ui/logo';
 </script>
 
-<div class="mb-4 flex flex-col items-center sm:mb-8">
+<div class="mb-4 flex flex-col items-center gap-2 sm:mb-8">
 	<Logo logoClass="w-16 h-16 sm:w-20 sm:h-20 mb-2" />
 	<h1 class="text-center text-2xl font-bold sm:text-3xl">Thoth</h1>
-	<p class="text-center text-sm text-gray-600 sm:text-base">Knowledge in Order</p>
+	<p class="text-center text-sm text-muted-foreground sm:text-base">Every title, in place.</p>
 </div>
