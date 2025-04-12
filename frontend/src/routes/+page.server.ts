cc3480ef5a3f1696c9561ca8d159c57cdd23acd9
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-12 23:11:10 +0800

    feat: add error page

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,14 +1,21 @@
 
 import { type TBook } from '$lib/types/types.js';
+import { error as errorPage } from '@sveltejs/kit';
 
 export async function load({ fetch }) {
-  const res = await fetch('http://localhost:8080/v1/books?page=1&limit=10');
-  const result = await res.json();
+  try {
+    const res = await fetch('http://localhost:8080/v1/books?page=1&limit=10');
+    const result = await res.json();
+    
+    if (res.status !== 200) {
+      errorPage(400, 'Failed to fetch books');
+    }
+    return {
+      books: result.books as TBook[],
+    };
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    throw errorPage(500, 'Something went wrong')
+  }
 
-  console.log(result.books)
-
-
-  return {
-    books: result.books as TBook[],
-  };
 }
