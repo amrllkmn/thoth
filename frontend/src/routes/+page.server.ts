46f904b9c17588aed6eeb062104c892281de3eea
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-12 23:14:24 +0800

    feat: use env variable

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 
 import { type TBook } from '$lib/types/types.js';
 import { error as errorPage } from '@sveltejs/kit';
+import { API_URL } from '$env/static/private';
 
 export async function load({ fetch }) {
   try {
-    const res = await fetch('http://localhost:8080/v1/books?page=1&limit=10');
+    const res = await fetch(`${API_URL}/v1/books?page=1&limit=10`);
     const result = await res.json();
     
     if (res.status !== 200) {
