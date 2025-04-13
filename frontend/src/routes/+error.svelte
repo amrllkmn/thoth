1750a4e553b229f6765bca7c0b173bc12a29425e
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-04-13 21:01:51 +0800

    feat: add error page

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -4,5 +4,7 @@
 
 <div class="mb-4 flex flex-col items-center gap-2 sm:mb-8">
 	<h1 class="text-center text-2xl font-bold sm:text-3xl">{page.error?.message} 🫠</h1>
-	<p class="text-center text-sm text-muted-foreground sm:text-base">Terribly sorry for this experience. Please contact the site owner.</p>
+	<p class="text-center text-sm text-muted-foreground sm:text-base">
+		Terribly sorry for this experience. Please contact the site owner.
+	</p>
 </div>
